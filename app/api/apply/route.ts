0249264bf64e48fbfc6250c01f83f3f2289d5c31
ncaroline <syokaucaroline7@gmail.com>
2026-06-25 23:43:45 +0300

    all

--- app/api/apply/route.ts
+++ app/api/apply/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import * as nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
 import { prisma } from "@/lib/prisma";
 import fs from "fs";
 import path from "path";
